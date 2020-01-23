06d7b7c9595fa8bbd3c3a5a1c27e433fae79e017
EliseChap <chaponelise@gmail.com>
2020-01-23 16:51:42 +0100

    first commit

---
+++ pokdemo/src/app/my-component/my-component.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from '../pokemon';
+
+@Component({
+  selector: 'app-my-component',
+  templateUrl: './my-component.component.html',
+  styleUrls: ['./my-component.component.css']
+})
+export class MyComponentComponent implements OnInit {
+  id: string = '';
+  pikachu: Pokemon  = new Pokemon("1","pikachu");
+  Dracaufeu: Pokemon  = new Pokemon("2","Dracaufeu");
+  Mewtwo: Pokemon  = new Pokemon("3","Mewtwo");
+  Lucario: Pokemon  = new Pokemon("4","Lucario");
+  Evoli: Pokemon  = new Pokemon("5","Evoli");
+
+  liste: Array<Pokemon> = [this.pikachu,this.Dracaufeu,this.Mewtwo,this.Lucario,this.Evoli];
+
+  selectedPoke;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ pokdemo/src/app/pokemon.ts
@@ -0,0 +1,8 @@
+export class Pokemon {
+    id: string = '';
+    name: string = '';
+    constructor(id, name) {
+        this.id=id;
+        this.name=name;
+     }
+}
